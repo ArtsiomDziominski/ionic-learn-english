c7eccfda95be4ada6deed039ffbbf4d892a83594
ArtemDeminski <artem.d@konomic.com>
2024-09-01 23:10:56 +0400

    upd name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'learn english',
+  appName: 'Daily Easy English',
   webDir: 'dist'
 };
 
