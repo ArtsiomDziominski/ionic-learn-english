99a3ef64d9b22fd2a0aae5205587c79faa6828db
ArtemDeminski <artem.d@konomic.com>
2024-09-22 12:11:12 +0400

    config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.learn.english',
   appName: 'Daily Easy English',
   webDir: 'dist'
 };
