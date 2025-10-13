2a4818dc7db53a0e628e01aedc93cae9a74acde3
ArtsiomDziominski <artsiomdziominski@gmail.com>
2025-10-13 21:40:38 +0400

    add google

--- src/type/article.d.ts
+++ src/type/article.d.ts
@@ -1,3 +1,12 @@
-export type Article = {id: string, title: string, description: string, body: string, img: string};
+export type Article = {
+    id: string,
+    title: string,
+    description: string,
+    body: string,
+    img: string,
+    publishedTime?: string,
+    modifiedTime?: string,
+    author?: string
+};
 
 export as namespace ARTICLE;
