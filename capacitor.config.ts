69c66e0f25c9ea409a582309390c6bcc6641fd6e
ArtsiomDziominski <artsiomdziominski@gmail.com>
2025-10-17 17:34:52 +0400

    fix back scroll

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.learn.english',
   appName: 'Daily Easy English',
-  webDir: 'dist'
+  webDir: 'dist',
+  ios: {
+    // Настройки для корректной работы iOS swipe back gesture
+    contentInset: 'automatic',
+    scrollEnabled: true
+  }
 };
 
 export default config;
